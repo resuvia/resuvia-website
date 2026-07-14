c77773efc6c4e980c409a8029ec20bdfd3e443a9
Utsav <resuviaofficial@gmail.com>
2026-07-14 20:02:14 +0530

    Navbar visible

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,20 +4,22 @@ export default function Navbar() {
   return (
     <nav
       style={{
-        background: "#ffffff",
-        padding: "18px 40px",
+        background: "red",
+        color: "white",
+        padding: "20px",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
+        borderBottom: "5px solid yellow",
       }}
     >
-      <h2>🚀 Resuvia</h2>
+      <h2 style={{ color: "white", margin: 0 }}>🚀 Resuvia</h2>
 
       <div>
-        <a href="#" style={{ marginRight: "20px" }}>Home</a>
-        <a href="#" style={{ marginRight: "20px" }}>Services</a>
-        <a href="#" style={{ marginRight: "20px" }}>Pricing</a>
-        <a href="#">Contact</a>
+        <a href="#" style={{ color: "white", marginRight: "20px" }}>Home</a>
+        <a href="#" style={{ color: "white", marginRight: "20px" }}>Services</a>
+        <a href="#" style={{ color: "white", marginRight: "20px" }}>Pricing</a>
+        <a href="#" style={{ color: "white" }}>Contact</a>
       </div>
     </nav>
   );
